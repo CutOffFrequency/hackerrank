cea596af37bf1114dc53de3430f02bb750dd138b
Erik Waters <erikj.waters@gmail.com>
2018-10-01 10:42:03 -0400

    algos applesAndOranges

---
+++ algos/applesAndOranges/applesAndOranges.js
@@ -0,0 +1,13 @@
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+  let fruitsInRange = [0, 0];
+  // if fruit falls within the target range, increment appropriate value
+  apples.forEach(d => {
+    if (a + d >= s && a + d <= t) fruitsInRange[0] += 1;
+  });
+  oranges.forEach(d => {
+    if (b + d >= s && b + d <= t) fruitsInRange[1] += 1;
+  });
+  fruitsInRange.forEach(score => console.log(score));
+}
+
+console.log(countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6]));
