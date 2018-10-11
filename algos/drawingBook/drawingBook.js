bc3656d8b81d1f2c50f56a33009c274c9382068d
Erik Waters <erikj.waters@gmail.com>
2018-10-11 18:57:04 -0400

    algos drawingBook

---
+++ algos/drawingBook/drawingBook.js
@@ -0,0 +1,12 @@
+function pageCount(n, p) {
+  // total page turns possible
+  let total = Math.floor(n / 2);
+  // from first page
+  let right = Math.floor(p / 2);
+  // from last page
+  let left = total - right;
+  // return the lowest
+  return Math.min(...[right, left]);
+}
+
+console.log(pageCount(10, 6));
