dd3eca04020b5afa95fd11a304c138708adb3a8e
Erik Waters <erikj.waters@gmail.com>
2018-10-19 15:38:58 -0400

    new solution, same problem

--- algos/climbingTheLeaderboard/climbingTheLeaderboard.js
+++ algos/climbingTheLeaderboard/climbingTheLeaderboard.js
@@ -1,55 +1,55 @@
-// I got pretty far with this attempt but it's more complex than it needs to be
-// I think I will try to combine both arrays into a set, then convert back
-// to an array and I can figure out the record placement by index
+// this previous solution timed out on 4 of 12 test cases
+//
+// function climbingLeaderboard(scores, alice) {
+//   // tally ranks until current score is found
+//   return alice.reduce(
+//     (acc, currentAlice) => {
+//       let i = 0;
+//       let tally = 1;
+//       // update tally
+//       while (currentAlice < scores[i]) {
+//         tally += scores[i] === scores[i - 1] ? 0 : 1;
+//         i++;
+//       }
+//       acc.ranks = [...acc.ranks, tally];
+//       return acc;
+//       // since alice's scores are ascending there is no need to update scores
+//       return acc;
+//     },
+//     { scores, ranks: [] }
+//   ).ranks;
+// }
+
+// simple, slightly optimized solution is still timing out on the same 4 cases
 
 function climbingLeaderboard(scores, alice) {
-  const flipArr = arr => arr.reduce((flip, current) => [current, ...flip], []);
-  // reverse alices scores so they are also descending
-  const aliceDescending = flipArr(alice);
-  // removes remaining scores until alice's next score is highest
-  const sliceOffRemainingScores = (acc, alice, prevHighest) => {
-    console.log(`${acc} ${alice} ${prevHighest}`);
-    const currentRecord = acc.highScores[0];
-    if (alice < currentRecord) {
-      acc.highScores = acc.highScores.slice(1);
-      if (currentRecord < prevHighest) {
-        acc.recordTally += 1;
+  // de dupe standing records
+  let scoresSet = new Set(scores);
+  // sort records ascending
+  let scoresArray = Array.from(scoresSet).sort((a, b) => a - b);
+  return alice.reduce(
+    (acc, current) => {
+      // if current score is greater than lowest record, slice until it isn't
+      while (current > acc.scores[0]) {
+        acc.scores = acc.scores.slice(1);
       }
-      return sliceOffRemainingScores(acc, alice, currentRecord);
-    }
-    return acc;
-  };
-  // iterate through alice's scores, rank, then return in the correct order
-  return flipArr(
-    aliceDescending.reduce(
-      (acc, score, i, alicesScores) => {
-        let newRank, highScoresIndex;
-        // if alice's score is greater than or equal to next high score
-        if (score >= acc.highScores[0]) {
-          // if alice's score is equal to previous high score
-          if (score === acc.prev || acc.prev === null) {
-            // add current tally value to alicesRanks
-            newRank = acc.recordTally;
-          } else {
-            // else increment tally
-            acc.recordTally += 1;
-          }
-        }
-        console.log(`${i} ${acc.alicesRanks} ${newRank}`);
-        acc.alicesRanks = [...acc.alicesRanks, newRank];
-        // if next high score is higher than previous, increment tally
-        if (acc.highScores[0] > acc.prev) acc.recordTally += 1;
-        // if alice has scores remaining and the next score is lower than the next record
-        if (alicesScores[i + 1] && alicesScores[i + 1] < acc.highScores[1])
-          // slice off remaining scores, incrementing tally
-          acc = sliceOffRemainingScores(acc, alicesScores[i + 1], acc.prev);
-        return acc;
-      },
-      { highScores: scores, prev: null, recordTally: 1, alicesRanks: [] }
-    ).alicesRanks
-  );
+      // if records have remaining elements
+      if (acc.scores.length > 0) {
+        // and current score is equal to lowest remaining record
+        if (current === acc.scores[0])
+          // rank is equal to remaining records length
+          acc.ranks = [...acc.ranks, acc.scores.length];
+        if (current < acc.scores[0])
+          // else it is equal to length plus one
+          acc.ranks = [...acc.ranks, acc.scores.length + 1];
+      } else {
+        // if there are no records remaining, player score is the new highest
+        acc.ranks = [...acc.ranks, 1];
+      }
+      return acc;
+    },
+    { scores: scoresArray, ranks: [] }
+  ).ranks;
 }
 
-console.log(
-  climbingLeaderboard([100, 100, 50, 40, 40, 20, 10], [5, 25, 50, 120])
-);
+console.log(climbingLeaderboard([2, 3, 4], [1, 1, 5, 5]));
