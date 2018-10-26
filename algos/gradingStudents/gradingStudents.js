6324d37122d4b88b4650466adbe2e7ad59361692
Erik Waters <erikj.waters@gmail.com>
2018-10-26 14:49:48 -0400

    algos/gradingStudents

---
+++ algos/gradingStudents/gradingStudents.js
@@ -0,0 +1,13 @@
+function gradingStudents(grades) {
+  let roundIfNeeded = grade => {
+    let intIndex, gradeSplit;
+    gradeSplit = grade.toString().split("");
+    intIndex = "3489".indexOf(gradeSplit[1]);
+    if (intIndex < 0) return grade;
+    if (intIndex < 2) return parseInt(gradeSplit[0] + 5, 10);
+    return (parseInt(gradeSplit[0], 10) + 1) * 10;
+  };
+  return grades.reduce((grades, grade) => {
+    return [...grades, grade < 38 ? grade : roundIfNeeded(grade)];
+  }, []);
+}
